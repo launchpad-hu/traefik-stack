97798349f1a42436ca18846a13b0f509f00ce73a
Bernát Kalló <kallo.bernat@gmail.com>
2023-04-24 15:06:31 +0200

    use overlay network

--- test.sh
+++ test.sh
@@ -1,7 +1,9 @@
 set -eo pipefail
 set -x
+ssh dokku@dev apps:destroy --force traefik || true
 ssh dokku@dev nginx:stop
 ssh dokku@dev apps:create traefik
+ssh dokku@dev proxy:set traefik null
 ssh dokku@dev builder:set traefik selected null
 ssh dokku@dev scheduler:set traefik selected swarm
 git push dokku HEAD
